24a74ba29b519cecaf0f509da22047b5b9d1aaf2
toreal <toreal@gmail.com>
2018-11-23 11:20:23 +0800

    q6

--- Assets/init.cs
+++ Assets/init.cs
@@ -12,7 +12,7 @@ public class init : MonoBehaviour {
 
         for (int i = 0; i < 10; i++)
         {
-            Rigidbody obj =Instantiate(prefab, new Vector3(i * 0.8F, 0.5f , 0), Quaternion.identity);
+            Rigidbody obj =Instantiate(prefab, new Vector3(i * 0.8F, 4.5f , 0), Quaternion.identity);
             if (i == 0)
                 startobj = obj;
         }
